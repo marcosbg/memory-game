f2d643e831b104a9dc8ed0f9f8c7ff1d3f9e0a87
marcos.bezerra <marcosbgomes@gmail.com>
2020-07-08 15:21:42 -0300

    :construction: Increase quantity of figures

--- src/app/mock-figures.ts
+++ src/app/mock-figures.ts
@@ -10,5 +10,7 @@ export const FIGURES: Figure[] = [
   { id: 17, name: 'Tartaruga' },
   { id: 18, name: 'Elefante' },
   { id: 19, name: 'Grilo' },
-  { id: 20, name: 'Cobra' }
+  { id: 20, name: 'Cobra' },
+  { id: 21, name: 'Coruja' },
+  { id: 22, name: 'Borboleta' }
 ];
