03753a68310ef66ccae315c3393c113af7a8a5e0
marcos.bezerra <marcosbgomes@gmail.com>
2020-07-09 14:44:00 -0300

    :construction: Add route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cards', component: CardsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
