3c05a55fdd5e178c25f82c0a6894d4e98bbedea7
marcos.bezerra <marcosbgomes@gmail.com>
2020-07-12 15:42:34 -0300

    :construction: Add mock items

--- src/app/mock-figures.ts
+++ src/app/mock-figures.ts
@@ -6,9 +6,11 @@ export const FIGURES: Figure[] = [
   { id: 13, name: 'Girafa' },
   { id: 14, name: 'Gato' },
   { id: 15, name: 'Cachorro' },
-  { id: 16, name: 'Hipopotamo' },
-  { id: 17, name: 'Tartaruga' },
-  { id: 18, name: 'Elefante' },
-  { id: 19, name: 'Coruja' },
-  { id: 20, name: 'Borboleta' }
+  { id: 16, name: 'Elefante' },
+  { id: 11, name: 'Leão' },
+  { id: 12, name: 'Macaco' },
+  { id: 13, name: 'Girafa' },
+  { id: 14, name: 'Gato' },
+  { id: 15, name: 'Cachorro' },
+  { id: 16, name: 'Elefante' },
 ];
